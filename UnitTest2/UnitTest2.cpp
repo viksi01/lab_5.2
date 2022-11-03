b795e41aeafc061c935b544b2d81a590611b2133
viksi01 <victoria.pyatnicka@gmail.com>
2022-11-03 14:53:28 +0200

    mastercommit1

---
+++ UnitTest2/UnitTest2.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5.2/lab_5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = A(1, 2, 3);
+			Assert::AreEqual(t, 1.35);
+		}
+	};
+}
